bd869b50335202782b514b86037e02f38407f6e7
Alexandre Dionizio <aleactis@hotmail.com>
2020-03-12 17:24:17 -0300

    Olá mundo em XAML

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPFUI
         {
             InitializeComponent();
         }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Olá {firstNameText.Text}");
+        }
     }
 }
